9d1940b9b9372e4d09633fe9a96bd1bcecc9a56d
Nathpobb <152171748+Nathpobb@users.noreply.github.com>
2023-12-26 23:48:41 +0700

    save

--- index_homework.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <head>
-        <meta charset="utf-8">
-    </head>
-    <body>
-        <h1><?php echo"hello world"</h1>
-    </body>
-</html>
--- php01/index.php
+++ php01/index.php
@@ -1,22 +1,52 @@
+<!DOCTYPE html>
 <html>
 
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="mystyle2.Css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        body {
+            background-image: url('bgyaya.jpg'); 
+            background-size: cover; 
+            background-repeat: no-repeat; 
+        }
+        .scrollable {
+            width: 150px;
+            height: 300px;
+            overflow-y: scroll;
+            scrollbar-arrow-color: blue;
+            scrollbar-face-color: #e7e7e7;
+            scrollbar-3dlight-color: #a0a0a0;
+            scrollbar-darkshadow-color: #888888;
+            background-color: white;
+        }
+    </style>
+    
+    
 </head>
-
 <center>
 <body>
-    <?php $multi_x = 2; $length_number = 24; ?>
+    <?php $multi_x = 2; ?>
     <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
-
-    <table>
-        <?php
-        for ($i = 1;$i<=$length_number;$i++){
-            echo "<tr><td>" .$multi_x.' X '.$i. ' = '.($multi_x*$i)."<br><br>\n";
-        }
-        
-?>
+    <div class="scrollable">
+        <table>
+            <tbody>
+                <?php
+                for ($i = 1; $i <= 24; $i++) {
+                ?>
+                    <tr>
+                        <td><?php echo $multi_x ?></td>
+                        <td>x</td>
+                        <td><?php echo $i ?></td>
+                        <td>=</td>
+                        <td><?php echo $multi_x * $i ?></td>
+                    </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
 </body>
-    </center>
-</html> 
+</center>
+</html>
