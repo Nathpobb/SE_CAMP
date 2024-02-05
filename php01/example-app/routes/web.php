dd2407ba0fff3e6a7adfb3bebf91f989baca9d1d
Nathpobb <152171748+Nathpobb@users.noreply.github.com>
2024-02-05 19:11:10 +0700

    saveee

--- php01/example-app/routes/web.php
+++ php01/example-app/routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\C_titles;
+use App\Http\Controllers\MyAuth;
 use App\Http\Controllers\MyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\C_titles;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,20 +18,16 @@ use App\Http\Controllers\C_titles;
 |
 */
 
+Route::get('/login', [MyAuth::class, 'login_view'])->name('login');
+Route::get('/register', [MyAuth::class, 'register_view']);
+Route::get('/logout', [MyAuth::class, 'logout_process']);
+Route::post('/login', [MyAuth::class, 'login_process']);
+Route::post('/register', [MyAuth::class, 'register_process']);
 
-Route::get('/', function () {
-    return view('welcome'); //welcome.blade.php
+Route::resource('titles', C_titles::class)->middleware('auth');
+Route::middleware('auth')->group(function(){
+    // auth first
 });
-
-Route::get('/my-route',function(){
-    return view('myfolder.mypage');
-});
-
-Route::post('/my-route', function(Request $req){
-    $data['myinput'] = $req->input('myinput');
-    return view('myroute',$data);
-});
-
 Route::get('/my-controller', [MyController::class, 'index']);
 
 Route::get('/my-controller2', 'App\Http\Controllers\MyController@index');
@@ -40,10 +38,23 @@ Route::namespace('App\Http\Controllers')->group(function(){
 
 Route::resource('/my-controller4', MyController::class);
 
-Route::resource('titles', C_titles::class);
 
-Route::get('/my-controller', [MyController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome'); // welcome.blade.php
+});
 
-Route::get('login',[MyAuth::class,'login_view']);
-Route::get('/register',[MyAuth::class,'register_view']);
-Route::get('/register',[MyAuth::class,'logout_process']);
+// use Illuminate\Http\Request;
+
+Route::get('/my-route', function(){
+    // return view('myroute');
+    //        Key    =>  Value
+    $data = ['val_a' => 'Hello World!'];
+    $data['val_b'] = "Laravel";
+    return view('myfolder.mypage',$data);
+});
+
+
+Route::post('/my-route', function(Request $req){
+    $data['myinput'] =  $req->input('myinput');
+    return view('myroute', $data);
+});
